cd0bab752002777eb7f3664ff7a60c1144074806
Your Name <you@example.com>
2025-07-25 15:17:17 +0800

    updates

--- src/main/java/com/bank/controller/BranchController.java
+++ src/main/java/com/bank/controller/BranchController.java
@@ -1,18 +1,24 @@
 package com.bank.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bank.mapper.BranchMapper;
 import com.bank.model.BranchDTO;
 import com.bank.service.IBranchService;
 import com.bank.validation.BranchValidation;
-import com.demo.exceptions.DemoAppException;
-import com.bank.mapper.BranchMapper;
 
 import lombok.AllArgsConstructor;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @AllArgsConstructor
 @RestController
